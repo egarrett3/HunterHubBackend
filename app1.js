1b34a1573254e5b7d922e52c988ddbf5690e6510
edwardg <egarrett3@fordham.edu>
2021-05-27 16:56:32 -0600

    pluralized users route

--- app1.js
+++ app1.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 
 const db = require('./config/keys').mongoURI
+const users = require('./routes/api/users');
 
 app.use(express.json());
 
@@ -12,6 +13,7 @@ mongoose
     .catch((err) => console.log(err))
 
 app.get('/',(req,res) => res.send('hello, we meet again'))
+app.use('/api/users', users);
  
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
