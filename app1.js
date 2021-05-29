e926bcddd1b47ba7f0a58173d3a764490149ebdb
edwardg <egarrett3@fordham.edu>
2021-05-28 18:54:12 -0600

    change port number

--- app1.js
+++ app1.js
@@ -18,5 +18,5 @@ mongoose
 app.get('/',(req,res) => res.send('hello, we meet again'))
 app.use('/api/users', users);
  
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
